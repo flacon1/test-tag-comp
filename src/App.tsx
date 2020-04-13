9a2c0c5a242ee02b26dce92bea17be8c52b0a732
Florin Jerdea <florin.jerdea@theaccessgroup.com>
2020-04-13 19:07:00 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {TagH1} from '@tag/tag-components-react-v3';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        xxx
+        <tag3-h1>Some interesting text...</tag3-h1>
+        <TagH1>xxx</TagH1>
       </header>
     </div>
   );
---
+++ src/global.ts
@@ -0,0 +1,8 @@
+import React from 'react';
+declare global {
+    namespace JSX {
+        interface IntrinsicElements {
+            'tag3-h1': any;
+        }
+    }
+  }
